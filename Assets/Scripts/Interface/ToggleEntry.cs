44564be5e1a38f6112803ffff6ddcc74e41870c2
Jacopo <fabrini.j@buas.nl>
2023-08-30 11:11:12 +0200

    GEOV-33: Bugfix, Missing info icon from ToggleEntry_PRFB

--- Assets/Scripts/Interface/ToggleEntry.cs
+++ Assets/Scripts/Interface/ToggleEntry.cs
@@ -4,5 +4,22 @@ using UnityEngine;
 
 public class ToggleEntry : MonoBehaviour
 {
- 
+    [SerializeField]
+    CustomToggle m_toggleEntry;
+    [SerializeField]
+    CustomButton m_infoButton;
+    [SerializeField]
+    GameObject m_toggleEntryVisual, m_infoButtonVisual;
+
+    private void Awake()
+    {
+        //TODO
+        //m_toggleEntry.onValueChanged.AddListener((b) => m_toggleEntryVisual.SetActive(b));
+        //m_infoButton.onClick.AddListener(VisualiseInfo);
+    }
+
+    void VisualiseInfo()
+    {
+        //TODO
+    }
 }
