b4e2449a53013154b9e968e126ab83d03f224ebc
Jacopo <fabrini.j@buas.nl>
2023-08-31 11:09:25 +0200

    GEOV-4 Alert Page Development

---
+++ Assets/Scripts/Interface/CloseWindow.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseWindow : MonoBehaviour
+{
+    [SerializeField]
+    CustomButton m_closeWindowButton;
+
+    [SerializeField]
+    GameObject m_window;
+
+    private void Awake()
+    {
+        m_closeWindowButton.onClick.AddListener(() => m_window.SetActive(false));
+    }
+}
