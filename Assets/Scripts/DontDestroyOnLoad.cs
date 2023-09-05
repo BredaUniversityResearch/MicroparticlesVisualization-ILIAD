7f9489376cadb178777ee9ebfab82ae46bbdb0e1
Jacopo Fabrini <JacopoFabrini@users.noreply.github.com>
2023-09-05 14:38:32 +0200

    GEOV-5: Settings Page Development

---
+++ Assets/Scripts/DontDestroyOnLoad.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace MSP2050.Scripts
+{
+	public class DontDestroyOnLoad : MonoBehaviour
+	{
+		// Use this for initialization
+		void Start()
+		{
+			DontDestroyOnLoad(gameObject);
+		}
+	}
+}
