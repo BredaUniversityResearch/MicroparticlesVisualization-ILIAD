2397981aede0d5ca34f93316d5eda3657c0e3f1b
Kevin <hutchinson.k@buas.nl>
2023-10-23 11:50:09 +0200

    GEOV-23: Added new test particle data, updated colour determination, flipped camera on so forward is -z.

--- Assets/Scripts/Aspects/ParticleUpdateAspect.cs
+++ Assets/Scripts/Aspects/ParticleUpdateAspect.cs
@@ -16,8 +16,9 @@ public readonly partial struct ParticleUpdateAspect : IAspect
 	public void SetTimeIndex(int a_index)
 	{
 		float depth = m_particleProperties.ValueRO.Value.Value.m_depths[a_index];
-		float t = math.abs(depth) / 100f;
-		m_colour.ValueRW.Value = new float4( t, t, 1f, 1f);
+		float rg = math.pow(1f - math.abs(depth) / 100f, 2);
+		float b = 1f - math.pow( math.abs(depth) / 100f, 2);
+		m_colour.ValueRW.Value = new float4( rg, rg, b, 1f);
 		m_transform.ValueRW.Position = new float3( (m_particleProperties.ValueRO.Value.Value.m_lons[a_index] - 10.476f) * m_xyScale, (m_particleProperties.ValueRO.Value.Value.m_lats[a_index] - 63.583f) * m_xyScale, depth * m_zScale);
 	}
 }
