84090e3f2895444cb0ca2972d850d15f33f5eb5e
Jeremiah van Oosten <oosten.j@buas.nl>
2023-11-08 14:53:58 +0100

    Add custom QuadTreeCameraMovement script that works with pointer callbacks. Add Location_Marker_Cursor for UnderSeaView script.

--- Assets/Scripts/UnderSeaView.cs
+++ Assets/Scripts/UnderSeaView.cs
@@ -1,9 +1,11 @@
 using Mapbox.Unity.Map;
+using Mapbox.Unity.Map.TileProviders;
+using Mapbox.Unity.MeshGeneration.Data;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-[RequireComponent(typeof(AbstractMap))]
-public class UnderSeaView : MonoBehaviour , IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
+[RequireComponent(typeof(AbstractMap), typeof(MeshCollider))]
+public class UnderSeaView : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
 {
     /// <summary>
     /// The cursor to use when hovering over the map.
@@ -20,19 +22,52 @@ public class UnderSeaView : MonoBehaviour , IPointerClickHandler, IPointerEnterH
     /// </summary>
     private AbstractMap map;
 
+    /// <summary>
+    /// Mesh collider for detecting pointer events.
+    /// </summary>
+    private MeshCollider meshCollider;
+
+    /// <summary>
+    /// Update the collider that is used for detecting pointer events.
+    /// </summary>
+    void UpdateMeshCollider()
+    {
+        var meshes = map.gameObject.GetComponentsInChildren<MeshFilter>();
+
+        var combine = new CombineInstance[meshes.Length];
+        for (int i = 0; i < meshes.Length; i++)
+        {
+            combine[i].mesh = meshes[i].sharedMesh;
+            combine[i].transform = meshes[i].transform.parent.localToWorldMatrix.inverse * meshes[i].transform.localToWorldMatrix;  // Matrix4x4.TRS(meshes[i].transform.localPosition, meshes[i].transform.localRotation, meshes[i].transform.localScale);
+        }
+
+        var mesh = meshCollider.sharedMesh ?? new Mesh();
+        
+        mesh.CombineMeshes(combine);
+
+        meshCollider.sharedMesh = mesh;
+    }
+
     void Start()
     {
         map = GetComponent<AbstractMap>();
-        // This is required to get the pointer events to work on the map.
-        //map.Terrain.SetElevationType(ElevationLayerType.TerrainWithElevation);
-        //map.Terrain.EnableCollider(true);
+        meshCollider = GetComponent<MeshCollider>();
+
+        UpdateMeshCollider();
+
+        map.TileProvider.ExtentChanged += TileProvider_ExtentChanged;
+    }
+
+    private void TileProvider_ExtentChanged(object sender, ExtentArgs e)
+    {
+        UpdateMeshCollider();
     }
 
     public void OnPointerClick(PointerEventData eventData)
     {
         if (eventData.clickCount == 2)
         {
-            Debug.Log("Zoom to under sea view.");
+            Debug.Log("Under Sea View: Double Click!");
         }
     }
 
