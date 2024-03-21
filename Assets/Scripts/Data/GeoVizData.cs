e26d720ceb99b3b76db38a818981aabd7d598c86
Kevin <hutchinson.k@buas.nl>
2024-03-21 15:56:17 +0100

    GEOV-199: Loading of NetCDF and JSON files

---
+++ Assets/Scripts/Data/GeoVizData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeoVizData
+{
+	public float[] lats;
+	public float[] lons;
+	public float[] zs;
+	public int entries_per_particle = -1;
+}
+
